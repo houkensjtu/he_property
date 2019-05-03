0804062b3319edc36b1dd5ba30df966d1d24d49a
Hou Ken / Bao Qian <houkensjtu@gmail.com>
2019-05-03 18:03:05 -0400

    Create rpcal.f

---
+++ rpcal.f
@@ -0,0 +1,26 @@
+      program helium
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      COMMON /CONSTA/G,R,TAO
+
+      P  = 10
+      T  = 5.0
+      RP = 10.0
+
+      do j=1,100
+      P = P * 1.5
+      call cpu_time(t1)
+      do i=1,1000000
+CC    P  = 1500000.0
+      call RPCALCUL(T,RP,P)
+CC    print *,T,RP,P
+      end do
+      call cpu_time(t2)
+
+      print *," "
+      print *,"Collapsed CPU time: ", t2-t1
+      print *,"P  = ", p
+      print *,"RP = ", rp
+      print *," "
+      end do
+
+      end program helium
