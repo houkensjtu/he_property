09881507a18064b143961c0db637121d3fab5efe
Qian Bao <qbao@Allentown.SHIAPD>
2019-07-11 17:45:26 -0400

    Helium density roughly agree with NIST confirmed.

--- rpcal.f
+++ rpcal.f
@@ -1,26 +1,28 @@
       program helium
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
       COMMON /CONSTA/G,R,TAO
-
-      P  = 10
-      T  = 5.0
+      
+      open(1, file='he.dat', status='replace')
+      
+      P  = 0.3d6
+      T  = 5.0d0
       RP = 10.0
-
-      do j=1,5
-      P = P * 1.5
-      call cpu_time(t1)
-      do i=1,3
-CC    P  = 1500000.0
-      call RPCALCUL(T,RP,P)
-CC    print *,T,RP,P
+      do i=1,2
+      T = 5.0d0
+      P=P+0.01d6
+        do j=1,30000
+        T = T + 0.01
+        PP = P/1.0d6 
+CC      call cpu_time(t1)
+CC      P  = 1500000.0
+        call RPCALCUL(T,RP,P)
+CC      print *,T,RP,P
+CC      call cpu_time(t2)
+CC      print *,"Collapsed CPU time: ", t2-t1
+        
+CC      print '("P  = ",f6.3 ,", T  = ", f8.3, ", RP = ", f9.4)',PP,T,RP
+        write (1,'(f6.3, ",", f8.3, ",", f9.4)') PP,T,RP
+        end do
+        print *,"Current i = ",i
       end do
-      call cpu_time(t2)
-
-      print *," "
-      print *,"Collapsed CPU time: ", t2-t1
-      print *,"P  = ", p
-      print *,"RP = ", rp
-      print *," "
-      end do
-
       end program helium
