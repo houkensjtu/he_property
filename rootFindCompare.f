49bafca12cc47c9174b691405a0172474a3cdb13
Qian Bao <qbao@Allentown.SHIAPD>
2019-07-12 17:29:56 -0400

    UP

--- rootFindCompare.f
+++ rootFindCompare.f
@@ -11,14 +11,8 @@
         T = T * 1.1
         call cpu_time(t1)
         do j=1,100000
-CC      P  = 1500000.0
           RP = RP0
           call RPCALCUL(T,RP,P)
-CC      print *,T,RP,P
-CC      print *,"Collapsed CPU time: ", t2-t1
-        
-CC      print '("P  = ",f6.3 ,", T  = ", f8.3, ", RP = ", f9.4)',PP,T,RP
-CC      write (1,'(f6.3, ",", f8.3, ",", f9.4)') PP,T,RP
         end do
         call cpu_time(t2)
         
